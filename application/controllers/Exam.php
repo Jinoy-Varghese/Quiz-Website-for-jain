451e9a7689bffa2e0bbc0eb917c0cd9b034c2516
Jinoy Varghese <jinoy.v2000@gmail.com>
2022-08-29 14:05:13 +0530

    last page

--- application/controllers/Exam.php
+++ application/controllers/Exam.php
@@ -30,7 +30,72 @@ class Exam extends CI_Controller
   {
 		$this->load->view('header');
 		$this->load->view('/exam/index');
-		$this->load->view('footer');  }
+		$this->load->view('footer'); 
+   }
+  public function exam_finish()
+   {
+     $this->load->view('header');
+     $this->load->view('/exam/exam_finish');
+     $this->load->view('footer'); 
+    }
+
+  public function submit_answer_process()
+  {
+
+   $id=$_SESSION['u_id'];
+
+
+   $limit=$this->input->post('limit')-1;
+
+
+   $mark=0;
+   for($i=1;$i<=$limit;$i++)
+   {
+      
+      $answer='answer'.$i;
+      $o_answer=$this->input->post($answer);
+
+      $this->db->select('*');
+      $this->db->from('exam_questions');
+      $this->db->where('id',$i);
+      $sql=$this->db->get();
+      foreach($sql->result() as $exam_check)
+      {
+        if($o_answer==$exam_check->answer)
+        {
+          $mark++;
+        }
+      }
+      
+
+    }
+    $my_mark=($mark*10)/$limit;
+
+    $exam_data=array('userid'=>$id,'marks'=>$my_mark);
+    $this->db->insert('exam_marks',$exam_data);
+
+    $this->session->set_flashdata('insert_success',"Sucessfully inserted");
+    redirect('Exam/exam_finish','refresh');
+
+ }
+ public function logout()
+  {
+    $_SESSION = array();
+
+    // If it's desired to kill the session, also delete the session cookie.
+    // Note: This will destroy the session, and not just the session data!
+    if (ini_get("session.use_cookies")) 
+    {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    session_unset();
+    session_destroy();
+    redirect('Main/login');
+  }
 }
 
 
---
+++ application/views/exam/exam_finish.php
@@ -0,0 +1,20 @@
+<style>
+    .container{
+margin-top:26vh;
+    }
+    body{
+        background-image:url("<?php echo base_url("assets/image/bg_maths.png") ?>");
+        
+    }
+
+
+</style>
+<div class="container d-flex align-items-center justify-content-center">
+    <div class="border shadow row col-6 p-5 d-flex justify-content-center bg-white">
+        <center>
+    <i class="fa-regular fa-circle-check mb-4 h1 text-success" style="font-size:4em"></i><br /> 
+    <span class="fs-3">Your quiz is over<br /> It is safe to close this window now<span>
+        <a href="<?php echo base_url("Exam/logout") ?>" class="text-decoration-none fs-4"> Logout <i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+</center>
+    </div>
+</div>
