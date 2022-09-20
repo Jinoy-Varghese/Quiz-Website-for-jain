f00bfc0a7cfad4de71a448e9aa14bd5f9d008bbe
joe <joejohnparakka@gmail.com>
2022-09-20 20:32:04 +0530

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -7,49 +7,22 @@
 
 
 	<div class="col-8 mt-5 ps-5 ">
-	
+
 		<div class="display-3 fw-bold ms-3" style="color:#552a6b">JAIN QUIZ 2022</div>
-			
-		
-		
-		<div class="display-2 fw-bold ps-3 mb-5 pb-5"style="color:#6E3C86"> SCHOOL OF CS AND IT</div>
-		
-		
+
+
+
+		<div class="display-2 fw-bold ps-3 mb-5 pb-5" style="color:#6E3C86"> SCHOOL OF CS AND IT</div>
 
 
 
 
-		<div class="container ">
-			<div class="row row-cols-1 row-cols-md-3 g-1 col-10  ">
-				<div class="col p-3  ">
-					<div class="card shadow" style="height=150">
-						<img src="<?php echo base_url('assets/image/trophy2.png'); ?>" height="250" style="margin-top:-50px;"/>
-						<div class="card-body mx-2">
-							<h5 class="card-title fw-bold ms-3">2ST</h5>
-							<p class="card-text">cash prize 15000</p>
-						</div>
-					</div>
-				</div>
-				<div class="col p-3 ">
-					<div class="card shadow" style="height=100">
-						<img src="<?php echo base_url('assets/image/trophy1.png'); ?>" height="300" style="margin-top:-100px;" />
-						<div class="card-body mx-2">
-							<h5 class="card-title fw-bold ms-3">1ST</h5>
-							<p class="card-text">cash prize 20000</p>
-						</div>
-					</div>
-				</div>
-				<div class="col p-3 ">
-					<div class="card shadow" style="height=60">
-						<img src="<?php echo base_url('assets/image/trophy3.png'); ?>" height="250" style="margin-top:-50px;"/>
-						<div class="card-body mx-2">
-							<h5 class="card-title fw-bold ms-3">3RD</h5>
-							<p class="card-text">cash prize 10000</p>
-						</div>
-					</div>
-				</div>
-			</div>
 
+
+		<div class="container p-3">
+
+			<p class="h3">Date: 22 september 2022</p>
+			<p class="h3">Venue: 7th floor</p>
 
 
 
@@ -59,63 +32,72 @@
 
 
 	</div>
-	<div class="col-4 p-3">
-		
-		<img class="img rounded float-start mt-5 pt-5" src="<?php echo base_url('assets/image/brain.png'); ?>" height="390px" />
+	<div class="col-md-4 row-sm-2 p-3">
+
+		<img class="img rounded float-start mt-5 pt-5" src="<?php echo base_url('assets/image/brain.png'); ?>"
+			height="390px" />
+
 
 	
-		<div class="container ">
-        <div class="col-md-12 text-center">
-            <button type="button" class="btn btn-primary col-12 mt-5 "><div class="h1"><a style="color:white; text-align: center;
-  text-decoration: none;  "href="<?php echo base_url('Main/login'); ?>" >Go to Login</a></div></button>
-           
-        </div>
-    </div>
+			
+				<button type="button" class="btn btn-primary col-12 mt-5 ">
+					<div class="h1"><a style="color:white; text-align: center;
+ 					 text-decoration: none;  " href="<?php echo base_url('Main/login'); ?>">Go to Login</a></div>
+				</button>
+
+			
+		
 
 	</div>
 
 </div>
 <style>
-	.img{
-		position:relative;
+	.img {
+		position: relative;
 		right: 40px;
 		top: 30px;
 	}
-	
+
 	body {
 		overflow-x: hidden;
-		overflow-y: hidden;
+
 		max-width: 100%;
-		background: rgb(255,240,163);
-		background: linear-gradient(130deg, rgba(255,240,163,1) 0%, rgba(254,175,238,1) 63%, rgba(255,118,254,1) 100%);
+
+		background: rgb(255, 240, 163);
+		background: linear-gradient(160deg, rgba(255, 118, 254, 1) 5%, rgba(254, 175, 238, 1) 53%, rgba(255, 118, 254, 1) 10%);
 	}
 
 	.btn {
-		background-color: #B338C4;
-	
-		position:relative;
-		top: 35px;
-		right: 50px;
-		
-		
+
+		background-color: #552A6B;
+        position: relative;
+		top: 35px; 
+		right: 30px;
+		width:100%;
+
+
+
 
 	}
-  
 
 
-	.btn:hover{
-		background-color: #552A6B;
-}
+
+	.btn:hover {
+		background-color: #52425b;
+	}
 
 
 
 	.card {
-		background: blue; 
+		background: blue;
 		border: 1px solid rgba(247, 249, 249, 0.4);
-		background: rgb(255,240,163);
-		background: linear-gradient(130deg, rgba(255,240,163,1) 34%, rgba(255,118,254,1) 100%);
-  		box-shadow: 3px 2px 5px #a6acaf;
+		background: rgb(255, 240, 163);
+		background: linear-gradient(130deg, rgba(255, 240, 163, 1) 34%, rgba(255, 118, 254, 1) 100%);
+		box-shadow: 3px 2px 5px #a6acaf;
 	}
 
+	.h3 {
+		color: #552A6B;
+	}
 
 </style>
