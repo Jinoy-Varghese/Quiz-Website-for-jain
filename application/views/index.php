d47b6919671267cdba61c8c59c33e0590942a007
joe <joejohnparakka@gmail.com>
2022-09-21 01:00:22 +0530

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -1,103 +1,72 @@
-<div class="  row mb-5 pt-3">
-
-
-
-
-
-
-
-	<div class="col-8 mt-5 ps-5 ">
-
-		<div class="display-3 fw-bold ms-3" style="color:#552a6b">JAIN QUIZ 2022</div>
-
-
-
-		<div class="display-2 fw-bold ps-3 mb-5 pb-5" style="color:#6E3C86"> SCHOOL OF CS AND IT</div>
-
-
-
-
-
-
-		<div class="container p-3">
-
-			<p class="h3">Date: 22 september 2022</p>
-			<p class="h3">Venue: 7th floor</p>
-
-
-
-		</div>
-
-
-
-
+<div class="row m-0">
+	<div class="col-md-8 col-sm-12">
+		<div class="display-4 fw-bold ms-3 mt-5" style="color:#552a6b">JAIN QUIZ 2022</div>
+		<div class="display-3 fw-bold ms-3" style="color:#6E3C86"> SCHOOL OF CS AND IT</div>
+		<div class="container">
+		<p class="h4 ms-2 mt-5 pt-5">Date: 22 september 2022</p>
+		<p class="h4 ms-2">Venue: 7th floor</p>
 	</div>
-	<div class="col-md-4 row-sm-2 p-3">
-
-		<img class="img rounded float-start mt-5 pt-5" src="<?php echo base_url('assets/image/brain.png'); ?>"
-			height="390px" />
+	</div>
 
 
 	
-			
-				<button type="button" class="btn btn-primary col-12 mt-5 ">
-					<div class="h1"><a style="color:white; text-align: center;
- 					 text-decoration: none;  " href="<?php echo base_url('Main/login'); ?>">Go to Login</a></div>
-				</button>
-
-			
+	<div class=" col-md-4 px-3 boximg">
+		<img class="img rounded float-start mt-5 pt-5 me-5 pe-5" src="<?php echo base_url('assets/image/brain.png'); ?> " />
+		<div class="row me-3 pe-3">
+		<button type="button" class="btn btn-primary "><a style="color:white; text-align: center;
+ 					 text-decoration: none;  " href="<?php echo base_url('Main/login'); ?>">Go to Login</a></button>
+		</div>
 		
+
 
 	</div>
 
 </div>
+
 <style>
-	.img {
-		position: relative;
-		right: 40px;
-		top: 30px;
+	img {
+		
+    max-width: 100%;
+    max-height: 100%;
+}
+	button{
+		width: 100%;
+  margin: 5px;
+	}
+	.btn{
+		background-color:#552A6B;
+	}
+	.btn:hover{
+		background-color:#6E3C86;
 	}
 
 	body {
-		overflow-x: hidden;
-
-		max-width: 100%;
+		margin: 0px;
+   min-height: 100%;
+   max-width: 100%;
 
 		background: rgb(255, 240, 163);
-		background: linear-gradient(160deg, rgba(255, 118, 254, 1) 5%, rgba(254, 175, 238, 1) 53%, rgba(255, 118, 254, 1) 10%);
+		background: linear-gradient(155deg, rgba(254, 175, 238, 1) 20%, rgba(254, 175, 238, 1)50%, rgba(255, 118, 254, 1) 55%);
 	}
+	
+	html {
+   margin: 0px;
+   height: 100%;
+   width: 100%;
+}
+@media (min-width: 992px) { 
 
-	.btn {
-
-		background-color: #552A6B;
-        position: relative;
-		top: 35px; 
-		right: 30px;
-		width:100%;
-
-
-
-
-	}
-
-
-
-	.btn:hover {
-		background-color: #52425b;
+	.boximg{
+    position: relative;
+	right: 120px;
+	top: 80px;
 	}
 
 
+ }
 
-	.card {
-		background: blue;
-		border: 1px solid rgba(247, 249, 249, 0.4);
-		background: rgb(255, 240, 163);
-		background: linear-gradient(130deg, rgba(255, 240, 163, 1) 34%, rgba(255, 118, 254, 1) 100%);
-		box-shadow: 3px 2px 5px #a6acaf;
-	}
 
-	.h3 {
-		color: #552A6B;
-	}
+  
+
 
 </style>
