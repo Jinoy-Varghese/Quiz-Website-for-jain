c818485bd5d8e5ba50b850739ee4d480f6945959
Jinoy Varghese <jinoy.v2000@gmail.com>
2022-09-22 00:42:46 +0530

    Update index.php

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -6,13 +6,21 @@
 <center class="mt-2 h2"> Admin Panel </center>
 
 <div class="container">
-    <div class="row d-flex justify-content-center">
-        <div class="col-md-2"></div>
+    <div class="row d-flex justify-content-center mb-4">
+        
         <a href="<?php echo base_url('Admin/view_participants'); ?>" class="btn btn-primary col-md-2 mr-1">View Participants</a>
-        <div class="col-md-1 mt-2"></div>
+        <div class="col-2"></div>
         <a href="<?php echo base_url('Admin/view_marks'); ?>" class="btn btn-primary col-md-2 ml-1">View Marks</a>
-        <div class="col-md-1 mt-2"></div>
-        <a href="<?php echo base_url('Admin/create_exam'); ?>" class="btn btn-primary col-md-2 ml-1">Add Questions</a>
-        <div class="col-md-2"></div>
+
     </div>
+
+    <div class="row d-flex justify-content-center">
+        
+    <a href="<?php echo base_url('Admin/create_exam'); ?>" class="btn btn-primary col-md-2 ml-1">Add Questions</a>
+    <div class="col-2"></div>
+    <a href="<?php echo base_url('Admin/view_questions'); ?>" class="btn btn-primary col-md-2 ml-1">View Questions</a>
+
+    </div>
+
+
 </div>
