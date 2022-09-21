ae8836e4c4449da0357846842ba01350dd5c8e77
joe <joejohnparakka@gmail.com>
2022-09-21 09:06:43 +0530

    removed prize add date

--- application/views/index.php
+++ application/views/index.php
@@ -1,8 +1,8 @@
 <div class="row m-0">
-	<div class="col-md-8 col-sm-12">
+	<div class="col-md-8 col-sm-12 mt-5 pt-5">
 		<div class="display-4 fw-bold ms-3 mt-5" style="color:#552a6b">JAIN QUIZ 2022</div>
 		<div class="display-3 fw-bold ms-3" style="color:#6E3C86"> SCHOOL OF CS AND IT</div>
-		<div class="container">
+		<div class="container pt-5">
 		<p class="h4 ms-2 mt-5 pt-5">Date: 22 september 2022</p>
 		<p class="h4 ms-2">Venue: 7th floor</p>
 	</div>
@@ -11,8 +11,8 @@
 
 	
 	<div class=" col-md-4 px-3 boximg">
-		<img class="img rounded float-start mt-5 pt-5 me-5 pe-5" src="<?php echo base_url('assets/image/brain.png'); ?> " />
-		<div class="row me-3 pe-3">
+		<img class="img rounded float-start mt-5 pt-4 me-5 pe-5" src="<?php echo base_url('assets/image/brain.png'); ?> " />
+		<div class="row me-3 pe-3 pt-4">
 		<button type="button" class="btn btn-primary "><a style="color:white; text-align: center;
  					 text-decoration: none;  " href="<?php echo base_url('Main/login'); ?>">Go to Login</a></button>
 		</div>
@@ -28,14 +28,16 @@
 		
     max-width: 100%;
     max-height: 100%;
+	
 }
 	button{
 		width: 100%;
-  margin: 5px;
+ 		 margin: 5px;
 	}
 	.btn{
 		background-color:#552A6B;
-	}
+		height: 55px;
+g	}
 	.btn:hover{
 		background-color:#6E3C86;
 	}
@@ -46,7 +48,7 @@
    max-width: 100%;
 
 		background: rgb(255, 240, 163);
-		background: linear-gradient(155deg, rgba(254, 175, 238, 1) 20%, rgba(254, 175, 238, 1)50%, rgba(255, 118, 254, 1) 55%);
+		background: linear-gradient(155deg, rgba(254, 175, 238, 1) 20%, rgba(254, 175, 238, 1)50%, rgba(255, 118, 254, 1) 45%);
 	}
 	
 	html {
